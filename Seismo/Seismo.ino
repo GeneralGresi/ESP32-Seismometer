a6ececa3f1f9b1410cfae8b3093392676c3420b9
GeneralGresi <general@gresihome.at>
2023-02-16 17:21:50 +0100

    Optimization, Multithreading

--- Seismo/Seismo.ino
+++ Seismo/Seismo.ino
@@ -1,11 +1,13 @@
 #include <WiFi.h>
 #include "credentials.h"
 #include <InfluxDbClient.h>
+#include "time.h"
+#include <esp_task_wdt.h>
 
 
 #define WIFI_TIMEOUT_DEF 30
-#define PERIOD_READ_MICROSECS 100                //Reading period 
-#define PERIOD_WRITE 100                //WRITING period 
+#define PERIOD_READ_MICROSECS 200                //Reading period 
+#define PERIOD_WRITE 10                //WRITING period 
 
 
 #define DEVICE "Sensor01"
@@ -17,19 +19,29 @@
 #define inputPinX100 39
 #define inputPinX10 34
 
+typedef struct{
+   unsigned long long timestamp;
+   int value;
+}Data;
 
-InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN);
-
-Point seismo("Seismometer");
 
 
-TaskHandle_t Task1;
 
 unsigned long lastReadTime;
 unsigned long lastWriteTime;
-
-
 unsigned long lastTimeSyncTime;
+  
+
+
+
+
+TaskHandle_t Task1;
+TaskHandle_t Task2;
+xQueueHandle xQueue;
+
+InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN);
+
+
 void syncToNTP() {
   if ((unsigned long)(millis() - lastTimeSyncTime) > 3600000) { //Sync every hour
     timeSync(TZ_INFO, "0.at.pool.ntp.org", "1.at.pool.ntp.org");
@@ -67,30 +79,64 @@ void setup() {
 
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-
-
-  seismo.addTag("device", DEVICE);
-  
-  
   delay(50);
 
-  client.setWriteOptions(WriteOptions().writePrecision(WritePrecision::US));
-  client.setWriteOptions(WriteOptions().batchSize(100));
+  
+  xQueue = xQueueCreate(100, sizeof(Point));
+
+ 
   client.setHTTPOptions(HTTPOptions().connectionReuse(true));
+  client.setWriteOptions(WriteOptions().writePrecision(WritePrecision::US).batchSize(100).useServerTimestamp(false));
   
   lastReadTime=micros();
-  lastWriteTime = lastTimeSyncTime = millis();
+  lastTimeSyncTime = lastWriteTime = millis();
+
+  esp_task_wdt_init(10, true); 
+  
+  
+  xTaskCreatePinnedToCore(
+      dataToQueue, /* Function to implement the task */
+      "ReadDataToQueue", /* Name of the task */
+      10000,  /* Stack size in words */
+      NULL,  /* Task input parameter */
+      0,  /* Priority of the task */
+      &Task1,  /* Task handle. */
+      0); /* Core where the task should run */
+      
+  xTaskCreatePinnedToCore(
+      postToInflux, /* Function to implement the task */
+      "postToInflux", /* Name of the task */
+      10000,  /* Stack size in words */
+      NULL,  /* Task input parameter */
+      2,  /* Priority of the task */
+      &Task2,  /* Task handle. */
+      1); /* Core where the task should run */
 
 }
 
-void postToInflux() {
-  syncToNTP();
-  if ((unsigned long)(millis() - lastWriteTime) > PERIOD_WRITE) {
-    if (!client.isBufferEmpty()) {
-      client.flushBuffer();
+
+void dataToQueue( void * parameter) {
+  esp_task_wdt_add(NULL);
+  const TickType_t xTicksToWait = pdMS_TO_TICKS(100);
+  BaseType_t xStatus;
+  Data dataPoint;
+  while (true) {
+      esp_task_wdt_reset();
+      if ((unsigned long)(micros() - lastReadTime) >= PERIOD_READ_MICROSECS) {
+        lastReadTime = micros();
+        dataPoint.timestamp = getMicros();
+        int inputValue = readInputs();
+        dataPoint.value = inputValue;
+        xStatus = xQueueSendToBack( xQueue, &dataPoint, xTicksToWait );  
     }
-    lastWriteTime = millis();
   }
+}
+
+
+unsigned long long getMicros() {
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return ((unsigned long long)((tv.tv_sec * 1000000LL) + tv.tv_usec));
 }
 
 
@@ -110,13 +156,32 @@ int readInputs() {
 }
 
 
-void loop() {  
-  if ((unsigned long)(micros() - lastReadTime) > PERIOD_READ_MICROSECS) {
-    int inputValue = readInputs();
-    seismo.clearFields();
-    seismo.addField("reading", inputValue);
-    client.writePoint(seismo);
-    lastReadTime = micros();
+
+void postToInflux(void * parameter) {
+  esp_task_wdt_add(NULL);
+  const TickType_t xTicksToWait = pdMS_TO_TICKS(100);
+  BaseType_t xStatus;
+  Point point("Seismometer");
+  point.addTag("device", DEVICE);
+  while(true) {
+    esp_task_wdt_reset();
+    syncToNTP();
+    Data dataPoint;
+    xStatus = xQueueReceive( xQueue, &dataPoint, xTicksToWait );
+    if(xStatus == pdPASS) {
+      point.clearFields();
+      point.addField("reading", dataPoint.value);
+      point.setTime(dataPoint.timestamp);
+      //Serial.println(dataPoint.timestamp + ": " + dataPoint.value);
+      client.writePoint(point);
+    }
+    if ((unsigned long)(millis() - lastWriteTime) > PERIOD_WRITE) {
+      //client.flushBuffer();
+      lastWriteTime = millis();
+    }
   }
-  postToInflux();
+}
+
+void loop() {
+  vTaskDelete(NULL);
 }
