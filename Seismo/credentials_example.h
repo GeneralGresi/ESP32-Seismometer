39a205edf9abee26b36f8d6a01de63f53f84fdd2
GeneralGresi <general@gresihome.at>
2024-01-14 11:31:57 +0100

    Added credentials example

---
+++ Seismo/credentials_example.h
@@ -0,0 +1,6 @@
+#define WIFI_SSID "SSID"
+#define WIFI_PASSWORD "PASSWORD"
+#define INFLUXDB_TOKEN "TOKEN"
+#define INFLUXDB_URL "http://yourinfluxinstance:8086"
+#define INFLUXDB_ORG "ORGANISATION"
+#define INFLUXDB_BUCKET "BUCKET"
